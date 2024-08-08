c66682b9023566e786981b7ac2d42e42137a9a84
Sultan Mukhtarov <suli419956@gmail.com>
2024-08-08 08:49:37 +0600

    Created a mui vigenere visual form and created git ignore in main directory.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,51 @@
+import {Box, Button, Container, Grid, TextField, Typography} from '@mui/material';
 
 const App = () => {
   return (
-      <>
-        <h1>Privet</h1>
-      </>
+    <>
+      <Container maxWidth="md">
+        <Box mt={5}>
+          <Typography variant="h4" align="center" mb={2}>
+            Vigenere Cipher
+          </Typography>
+          <Grid container spacing={2} justifyContent="center">
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="Password"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="Message"
+                multiline
+                rows={4}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Button variant="contained">
+                Encode
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Button variant="contained">
+                Decode
+              </Button>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label="Text"
+                multiline
+                rows={4}
+                disabled
+              />
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </>
   );
 };
 
